a197ef2b18f6c60bf1e7f5e16864055587eaa169
Chunlei Liu <40068295+liuchunlei0430@users.noreply.github.com>
2023-03-25 14:32:09 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python main.py --lr 0.02 --epochs 10 --batch_size 256 --hidden_modality1 185 --hidden_modality2 30  --modality1_path "./data/Rama/rna.h5"  --modality2_path "./data/Rama/adt.h5"  --cty "./data/Rama/cty.csv" --batch "./data/Rama/batch.csv" --dataset "Rama" --modality1 "rna" --modality2 "adt"
+
+python main.py --lr 0.02 --epochs 10 --batch_size 256 --hidden_modality1 185 --hidden_modality2 30  --modality1_path "./data/Rama/rna.h5"  --modality2_path "./data/Rama/adt.h5"  --cty_path "./data/Rama/cty.csv" --batch_path "./data/Rama/batch.csv" --dataset "Rama" --modality1 "rna" --modality2 "adt"
